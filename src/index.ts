a3a77d9707279ac484f7dde0d8f40daaba9960ca
JK Gunnink <jgunnink@gmail.com>
2025-02-03 14:56:30 +0800

    add html page with loader and shorten response with flash model

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,16 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const apiKey = process.env.GEMINI_API_KEY;
 
 export const aiTechSupport = async (req: Request, res: Response) => {
+  // Support CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    res.statusCode = 200;
+    res.end();
+    return;
+  }
+
   if (!req.body.message) {
     const response = "No message provided";
     res.statusCode = 400;
@@ -14,12 +24,12 @@ export const aiTechSupport = async (req: Request, res: Response) => {
   const genAI = new GoogleGenerativeAI(apiKey);
 
   const model = genAI.getGenerativeModel({
-    model: "gemini-exp-1206",
+    model: "gemini-2.0-flash-exp",
     systemInstruction:
       "You are an IT support person helping user resolve problems. When a problem comes in, provide a few troubleshooting steps to help them resolve",
   });
 
-  const generationConfig = { maxOutputTokens: 8192, responseMimeType: "text/plain" };
+  const generationConfig = { maxOutputTokens: 1024, responseMimeType: "text/plain" };
 
   const chatSession = model.startChat({
     generationConfig,
